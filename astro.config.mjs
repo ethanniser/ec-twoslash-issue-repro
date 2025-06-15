54593190e1d87eadf62ad7a353e818095326d59e
Ethan Niser <ethan@niser.com>
2025-06-14 23:42:05 -0700

    issue repro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import expressiveCode from "astro-expressive-code";
+import ecTwoSlash from "expressive-code-twoslash";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: "https://example.com",
+	integrations: [
+		sitemap(),
+		expressiveCode({
+			plugins: [ecTwoSlash()],
+		}),
+	],
 });
+
---
+++ src/content/blog/example.md
@@ -0,0 +1,16 @@
+---
+title: 'Example'
+description: 'Lorem ipsum dolor sit amet'
+pubDate: 'Jul 08 2022'
+heroImage: '../../assets/blog-placeholder-3.jpg'
+---
+
+```ts twoslash
+const foo = "bar";
+```
+
+This causes the whole page to go blank with no error (uncomment out to see)
+```ts twoslash
+import { useState } from 'react'
+const foo = useState(0)
+```
